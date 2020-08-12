8addfce17c17a49e9109f3b87ac6a2b032eacb2d
thinkkniht <td66666666@qq.com>
2020-08-12 22:54:23 +0800

    老外，gun！

---
+++ gaojingdu3.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+using namespace std;
+int main()
+{
+	char a1[101],b1[101];
+	int a[101],b[101],c[10001],lena,lenb,lenc,i,j,x;
+	memset(a,0,sizeof(a));
+	memset(b,0,sizeof(b));
+	memset(c,0,sizeof(c));
+	//scanf("%s",a1);
+	//scanf("%s",b1);
+	gets(a1);
+	gets(b1);
+	lena=strlen(a1);lenb=strlen(b1);
+	//cout<<a1<<""<<b1;
+	cout<<a1[1]<<endl;
+	for (i=0;i<=lena-1;i++) a[lena-i]=int(a1[i]-48);
+	for (i=0;i<=lenb-1;i++) b[lenb-i]=b1[i]-48;
+	//printf("%d,%d,%d",a1[3],a1[2],a1[1]);
+	cout<<a1[3]<<a1[2]<<a[1]<<"======"<<endl;
+	for (i=1;i<=lena;i++)
+	{
+		x=0;
+		for(j=1;j<=lenb;j++)
+		{
+			c[i+j-1]=a[i]*b[j]+x+c[i+j-1];
+			x=c[i+j-1]/10;
+			c[i+j-1]%=10;
+		}
+		c[i+lenb]=x;
+	}
+	lenc=lena+lenb;
+/*	for(i=1;i<=lenc;i++)
+	cout<<c[lenc];*/
+	while (c[lenc]==0&&lenc>1)
+	lenc--;
+	for (i=lenc;i>=1;i--)
+	cout<<c[i];
+	cout<<endl;
+	return 0;
+}
